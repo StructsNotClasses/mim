805f122381c4a79360782b6ed80c532249553391
ClassesNotStructs <john.casey184@gmail.com>
2021-12-14 16:10:34 -0800

    started work on tree

--- filebrowse.go
+++ filebrowse.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "fmt"
     "log"
     "os"
     "path/filepath"
@@ -27,42 +26,33 @@ func (s *SongList) addFile(path string, info os.FileInfo, err error) error {
     return nil
 }
 
-func storeFileTree(parent_dir string, song_list_file string) {
-    file, err := os.Create(song_list_file)
+func storeFileTree(parent_dir string, songListFile string) {
+    file, err := os.Create(songListFile)
     if err != nil {
-        fmt.Println("unable to open file", song_list_file)
         log.Fatal(err)
     }
 
-    song_list := SongList([]Song{})
-    filepath.Walk(parent_dir, song_list.addFile)
-    bytes, err := json.MarshalIndent(song_list, "", "  ")
+    s := SongList([]Song{})
+    filepath.Walk(parent_dir, s.addFile)
+    bytes, err := json.MarshalIndent(s, "", "  ")
     if err != nil {
-        fmt.Println("unable to convert song list to json")
         log.Fatal(err)
     }
 
     _, err = file.Write(bytes)
     if err != nil {
-        fmt.Println("unable to write song list to file", song_list_file)
         log.Fatal(err)
     }
 }
 
-func openSongList(path string) (SongList, error) {
+func createSongList(path string) (SongList, error) {
     s := SongList([]Song{})
 
     bytes, err := ioutil.ReadFile(path)
     if err != nil {
-        fmt.Println("unable to open or read file", path)
         return s, err
     }
 
     err = json.Unmarshal(bytes, &s)
-    if err != nil {
-        fmt.Println("unable to convert json to song list")
-        return s, err
-    }
-
-    return s, nil
+    return s, err
 }
---
+++ main.go
@@ -0,0 +1,239 @@
+package main
+
+import (
+    //"bufio"
+    //"strings"
+    //"os"
+    "strconv"
+    "os/exec"
+    "log"
+    "fmt"
+    "time"
+    "math/rand"
+    "io"
+    "errors"
+    gnc "github.com/rthornton128/goncurses"
+)
+
+const INT32MAX = 2147483647
+const PARENT_DIRECTORY = "/mnt/music"
+const SONG_LIST_FILE = "/mnt/music/music_player/songs.json"
+
+type Client struct {
+    backgroundWindow *gnc.Window
+    infoWindow *gnc.Window
+    treeWindow *gnc.Window
+    commandInputWindow *gnc.Window
+    commandOutputWindow *gnc.Window
+}
+
+type SongTree struct {
+    songs SongList
+    currentIndex int32
+    currentAtTop int32
+}
+
+//the writing end of the fifo pipe has to be opened only after the reading end is opened
+func main() { 
+    //current behavior is to regenerate the song list each run. probably needs to change
+    storeFileTree(PARENT_DIRECTORY, SONG_LIST_FILE) 
+
+    //open the entire song list
+    songs, err := createSongList(SONG_LIST_FILE)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    backgroundWindow, err := gnc.Init()
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer gnc.End()
+
+    gnc.CBreak(true)
+    gnc.Echo(false)
+    backgroundWindow.Keypad(true)
+
+    playAll(songs, createGuiClient(backgroundWindow))
+}
+
+func createGuiClient(scr *gnc.Window) Client {
+    totalHeight, totalWidth := scr.MaxYX()
+    leftToRightRatio := 0.6
+    COMMAND_INPUT_HEIGHT := 6
+    COMMAND_OUTPUT_HEIGHT := 10
+    BORDER_WIDTH := 1
+    
+    var topWindowHeight int = totalHeight - COMMAND_INPUT_HEIGHT - COMMAND_OUTPUT_HEIGHT - 4 * BORDER_WIDTH
+    var infoWindowWidth int = int(leftToRightRatio * float64(totalWidth)) - 2 * BORDER_WIDTH
+    var commandWindowsWidth int = infoWindowWidth
+    var treeWindowWidth int = totalWidth - infoWindowWidth - 3 * BORDER_WIDTH
+
+    //create the window that displays information about the current song
+    infoWindow, err := gnc.NewWindow(topWindowHeight, infoWindowWidth, BORDER_WIDTH, BORDER_WIDTH)
+    if err != nil {
+        log.Fatal(err)
+    }
+    infoWindow.ScrollOk(true)
+
+    //create the window that holds the song tree
+    treeWindow, err := gnc.NewWindow(totalHeight - 2 * BORDER_WIDTH, treeWindowWidth, BORDER_WIDTH, infoWindowWidth + 2 * BORDER_WIDTH)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    //create the window that allows user input
+    commandInputWindow, err := gnc.NewWindow(COMMAND_INPUT_HEIGHT, commandWindowsWidth, topWindowHeight + COMMAND_OUTPUT_HEIGHT + 3 * BORDER_WIDTH, BORDER_WIDTH)
+    if err != nil {
+        log.Fatal(err)
+    }
+    commandInputWindow.ScrollOk(true)
+
+    //create the window that holds command output
+    commandOutputWindow, err := gnc.NewWindow(COMMAND_OUTPUT_HEIGHT, commandWindowsWidth, topWindowHeight + 2 * BORDER_WIDTH, BORDER_WIDTH)
+    if err != nil {
+        log.Fatal(err)
+    }
+    commandOutputWindow.ScrollOk(true)
+
+    scr.Box('|', '-')
+    scr.VLine(1, infoWindowWidth + BORDER_WIDTH, '|', totalHeight - 2 * BORDER_WIDTH)
+    scr.HLine(topWindowHeight + BORDER_WIDTH, 1, '=', infoWindowWidth)
+    scr.HLine(topWindowHeight + COMMAND_OUTPUT_HEIGHT + 2 * BORDER_WIDTH, 1, '=', infoWindowWidth)
+    scr.Refresh()
+
+    return Client{
+        scr,
+        infoWindow,
+        treeWindow, 
+        commandInputWindow, 
+        commandOutputWindow,
+    }
+}
+
+func playAll(songs SongList, client Client) {
+    //check if windows are nil
+    if client.backgroundWindow == nil || client.infoWindow == nil || client.commandOutputWindow == nil || client.treeWindow == nil || client.commandInputWindow == nil {
+        log.Fatal(errors.New("Nil pointer to window provided for playback, quitting..."))
+    }
+
+    rand.Seed(time.Now().UnixNano())
+    if len(songs) > INT32MAX {
+        log.Fatal(fmt.Sprintf("Cannot play more than %d songs at a time.", INT32MAX))
+    }
+
+    user_input_ch := make(chan gnc.Key)
+    go takeUserInputIntoChannel(client.backgroundWindow, user_input_ch)
+
+    user_bs := []byte{}
+    exit_program := false
+
+    st := SongTree{songs, 0, 0}
+
+    for !exit_program {
+        st.currentIndex = rand.Int31n(int32(len(songs)))
+        drawTree(&st, client.treeWindow)
+        notifier := make(chan int)
+        remote := playFileWithMplayer(songs[st.currentIndex].Path, notifier, client.infoWindow)
+        playback_complete := false
+        for !playback_complete {
+            select {
+            case user_b := <- user_input_ch:
+                user_bs = append(user_bs, byte(user_b))
+                client.commandInputWindow.AddChar(gnc.Char(user_b))
+                client.commandInputWindow.Refresh()
+                if user_b == '\n' || user_b == '\r' {
+                    switch string(user_bs) {
+                    case "exit\n":
+                        exit_program = true
+                        remote.SendBytes([]byte("quit\n"))
+                    case "current song info\n":
+                        client.commandOutputWindow.Print("\nPlaying " + songs[st.currentIndex].Name)
+                        client.commandOutputWindow.Refresh()
+                    default:
+                        if string(user_bs[0:4]) == "send" {
+                            client.commandOutputWindow.Print("\nSending '" + string(user_bs[5:len(user_bs) - 1]) + "'")
+                            client.commandOutputWindow.Refresh()
+                            mplayerCommand := user_bs[5:]
+                            remote.SendBytes(mplayerCommand)
+                        } else if string(user_bs[0:4]) == "play" {
+                            indexBytes := user_bs[5:]
+                            index, _ := strconv.Atoi(string(indexBytes))
+                            remote.SendBytes([]byte("quit\n"))
+                            _ = <- notifier
+                            remote = playFileWithMplayer(songs[index].Path, notifier, client.infoWindow)
+                        } else {
+                            remote.SendBytes(user_bs)
+                        }
+                    }
+                    user_bs = []byte{}
+                }
+            case notification := <- notifier:
+                if notification == 1 {
+                    playback_complete = true
+                }
+            }
+            client.backgroundWindow.Refresh()
+        }
+    }
+}
+
+func drawTree(tree *SongTree, win *gnc.Window) {
+    win.Erase()
+    hog, _ := win.MaxYX()
+    height := int32(hog)
+    delta := tree.currentAtTop - tree.currentIndex
+    if delta > height {
+        tree.currentAtTop = tree.currentIndex - height + 1
+    } else if delta < 0 {
+        tree.currentAtTop = tree.currentIndex
+    }
+    //for i := currentAtTop; i < 
+    for i := tree.currentAtTop; i < tree.currentAtTop + height - 1; i++ {
+        song := tree.songs[i]
+        if i == tree.currentIndex {
+            win.AttrOn(gnc.A_STANDOUT)
+            win.AttrOff(gnc.A_NORMAL)
+            win.Print(song.Name + "\n")
+            win.AttrOff(gnc.A_STANDOUT)
+            win.AttrOn(gnc.A_NORMAL)
+        } else {
+            win.Print(song.Name + "\n")
+        }
+    }
+    win.Refresh()
+}
+
+func takeUserInputIntoChannel(window *gnc.Window, ch chan gnc.Key) {
+    for {
+        ch <- window.GetChar()
+    }
+}
+
+// run mplayer command "mplayer -slave -vo null <song path>"
+// the mplayer runner should send 1 to notify_ch when it completes playback. otherwise, nothing should be sent
+func playFileWithMplayer(file string, notifier chan int, outWindow *gnc.Window) Remote {
+    cmd := exec.Command("mplayer", 
+        "-slave", "-vo", "null", "-quiet", file)   
+
+    pipe, err := cmd.StdinPipe()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    go runWithWriter(cmd, BasicWindowWriter{outWindow}, notifier)
+
+    return Remote{pipe}
+}
+
+func runWithWriter(cmd *exec.Cmd, w io.WriteCloser, notifier chan int) {
+    cmd.Stdout = w
+
+    err := cmd.Run()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    notifier <- 1
+    w.Close()
+}
